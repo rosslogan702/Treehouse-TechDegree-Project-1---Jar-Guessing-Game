9ffbd449fdb7f0d148addc892b065d624f96fa72
Ross Logan <rosslogan10x@gmail.com>
2016-12-22 14:56:13 +0000

    Code changes after peer review

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -1,35 +1,31 @@
-import java.io.Console;
-
 public class Game {
   
-  private Console console = System.console();
   private Jar mJar;
   boolean mGuessedCorrectly = false;
-  private int mNumberOfGuesses=0;
   
   public void setup(String itemName, String maxItems) {
     mJar = new Jar(itemName, Integer.parseInt(maxItems));
     mJar.fill(Integer.parseInt(maxItems));
   }
   
-  public boolean applyGuess(int guess){
-    if(guess > mJar.getMaxItems()){
-      /**I understand that I have deviated from the spec here in terms of the message printed out
-      however I think that this is the correct message to display as the correct number of items
-      could be the max value in which case the guess would not be less than the max value **/
-      System.out.println("Your guess must be less than or equal to " + mJar.getMaxItems());
-      return mGuessedCorrectly;
-    }
+  public boolean isGuessOverMaxLimit(int guess) {
+    return (guess > mJar.getMaxItems());
+  }
+  
+  public boolean isTooHigh(int guess){
+    return (guess > mJar.getItemsInJar());
+  }
+  
+  public boolean isTooLow(int guess){
+    return (guess < mJar.getItemsInJar());
+  }
+  
+  public boolean isGuessedCorrectly(int guess){
     if(guess == mJar.getItemsInJar()){
-      mGuessedCorrectly = true;
-      } else if (guess > mJar.getItemsInJar()){
-        System.out.println("Your guess is too high!");
-    } else {
-        System.out.println("Your guess is too low!");
+      mGuessedCorrectly=true;
     }
-      mNumberOfGuesses++;
-      return mGuessedCorrectly;
-    }
+    return mGuessedCorrectly;
+  }
   
   public boolean guessedCorrectly() {
     return mGuessedCorrectly;
@@ -39,7 +35,10 @@ public class Game {
    return mJar; 
   }
   
-  public int getNumberOfGuesses(){
-    return mNumberOfGuesses;
+  public static void main (String [] args){
+    Game game = new Game();
+    Prompter gamePrompter = new Prompter(game);
+    gamePrompter.play();
+    gamePrompter.shutdown();
   }
 }
--- src/main/java/Prompter.java
+++ src/main/java/Prompter.java
@@ -1,45 +1,78 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Prompter {
   private Game mGame;
   private int numberOfGuesses;
-  private Scanner scanner = new Scanner(System.in);
+  private BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
   
   public Prompter(Game game){
    this.mGame = game; 
   }
   
   public void administratorSetup(){
-      System.out.println("Pleae enter the item name: ");
-      String itemName = scanner.nextLine();
-      System.out.println("What is the maximum number of " 
-                        + itemName + "s" + " that can fit in the jar " + ": ");
-      String maxItems = scanner.nextLine();
+    try {
+      System.out.print("Pleae enter the item name: ");
+      String itemName = br.readLine();
+      System.out.print("What is the maximum number of " 
+                        + itemName + " that can fit in the jar " + ": ");
+      String maxItems = br.readLine();
       mGame.setup(itemName, maxItems);
+      
+    } catch (IOException ioe){
+      System.out.println("Error when reading input from console! Exiting!");
+      System.exit(-1);
+    }
     
   }
   
- public boolean promptForGuess(){
-    System.out.println("How many " + mGame.getJar().getItemName() + "s" 
-                            + " are in the jar?" + " Pick a number between 1 and " +
-                                            mGame.getJar().getMaxItems() + ": ");
-    String guessAsString = scanner.nextLine();
+ public int promptForGuess(){
+  System.out.println("How many " + mGame.getJar().getItemName() + "s" 
+                     + " are in the jar?" + " Pick a number between 1 and " +
+                     mGame.getJar().getMaxItems() + ": ");
+   String guessAsString="";
+   try {
+   guessAsString = br.readLine();
+  } catch (IOException ioe){
+    System.out.println("Error reading in data from the console! Exiting!");
+    System.exit(-1);
+   }
     int guess = Integer.parseInt(guessAsString);
-    return mGame.applyGuess(guess);
+    return guess;
+ }
+  
+  public void checkGuess(int guess){
+      if(mGame.isGuessOverMaxLimit(guess)){
+        System.out.println("Your guess must be less than or equal to " + mGame.getJar().getMaxItems());
+      } else if (mGame.isTooLow(guess)){
+        numberOfGuesses++;
+        System.out.println("Your guess is too low!");
+      } else if (mGame.isTooHigh(guess)){
+        numberOfGuesses++;
+        System.out.println("Your guess is too high!");
+      } else if (mGame.isGuessedCorrectly(guess)){
+        numberOfGuesses++;
+        System.out.println("You got it in " + numberOfGuesses + " attempt(s)");
+      }
   }
   
   public void play(){
     administratorSetup();
     while(!mGame.guessedCorrectly()){
-      promptForGuess();
-      numberOfGuesses++;
+      checkGuess(promptForGuess());
     }
-    System.out.println("You got it in " + mGame.getNumberOfGuesses() + " attempt(s)");
   }
   
-  public static void main (String [] args){
-    Game game = new Game();
-    Prompter gamePrompter = new Prompter(game);
-    gamePrompter.play();
+  public void shutdown(){
+      if(br !=null){
+        try {
+          br.close();
+        } catch (IOException ioe){
+          ioe.printStackTrace();
+          System.exit(-1);
+        }
+    }
+   
   }
 }
